17f8da3c9d06fce1f185b323458886e7881c6dad
Alessandro Pappalardo <alessandro1.pappalardo@mail.polimi.it>
2016-12-04 13:36:13 -0600

    [Versioning] Complete implementation of get commits diff data by adding a new data class and the output message

---
+++ src/main/scala-2.11/edu/uic/cs474/project/Message.scala
@@ -0,0 +1,13 @@
+package edu.uic.cs474.project
+
+import edu.uic.cs474.project.downloading.DiffData
+
+/**
+  * Created by Alessandro on 02/12/16.
+  */
+//A trait representing messages
+sealed trait Message
+
+case class GetCommitsDiffDataMap(repoPath: String, oldCommit: String, newCommit: String) extends Message
+
+case class SendCommitsDiffDataMap(diffDataMap: Map[(String, String), List[DiffData]]) extends Message
--- src/main/scala-2.11/edu/uic/cs474/project/downloading/ProjectVersionManager.scala
+++ src/main/scala-2.11/edu/uic/cs474/project/downloading/ProjectVersionManager.scala
@@ -4,6 +4,7 @@ import java.io.{File, OutputStream}
 import java.lang.Iterable
 
 import akka.actor.Actor
+import edu.uic.cs474.project.{GetCommitsDiffDataMap, SendCommitsDiffDataMap}
 import org.eclipse.jgit.annotations.NonNull
 import org.eclipse.jgit.api.Git
 import org.eclipse.jgit.diff.{DiffConfig, DiffEntry, DiffFormatter, Edit}
@@ -12,6 +13,7 @@ import org.eclipse.jgit.revwalk.{FollowFilter, RevCommit, RevTree, RevWalk}
 import org.eclipse.jgit.storage.file.FileRepositoryBuilder
 import org.eclipse.jgit.treewalk.{AbstractTreeIterator, CanonicalTreeParser}
 import org.eclipse.jgit.util.io.{DisabledOutputStream, NullOutputStream}
+import sun.text.normalizer.Replaceable
 
 import scala.collection.JavaConverters._
 import scala.sys.process.Process
@@ -23,8 +25,8 @@ import scala.sys.process.Process
 class ProjectVersionManager extends Actor {
 
   override def receive: Receive = {
-    case GetCommitsDiffMap(repoPath, oldCommit, newCommit) =>
-      getDiffData(repoPath, oldCommit, newCommit)
+    case GetCommitsDiffDataMap(repoPath, oldCommit, newCommit) =>
+      sender ! SendCommitsDiffDataMap(getDiffDataList(repoPath, oldCommit, newCommit))
   }
 
   /**
@@ -32,17 +34,27 @@ class ProjectVersionManager extends Actor {
     * @param path path to the repository
     * @param oldCommit old commit hash
     * @param newCommit new commit hash
-    * @return a map with (oldPath, newPath) as key and List[Edit] as value
+    * @return a map with (oldPath, newPath) as key and List[DiffData] as value
     */
-  def getDiffData(path: String, oldCommit: String, newCommit: String): Map[(String, String), List[Edit]] = {
+  def getDiffDataList(path: String, oldCommit: String, newCommit: String): Map[(String, String), List[DiffData]] = {
     val repo: Repository = getRepoFromPath(path)
     val formatter: DiffFormatter = customDiffFormatter(repo)
     val diffData = getDiffEntries(formatter, repo, oldCommit, newCommit)
       .map(diffEntry =>
         //key for the edit map made by tuple (oldPath, newPath), since a file can be renamed
         ((diffEntry.getOldPath, diffEntry.getNewPath),
-          //value for the map, a list of jgit edit
-          formatter.toFileHeader(diffEntry).toEditList.asScala.toList))
+          //value for the map, a list of diffdata (with empty edits removed)
+          formatter
+            .toFileHeader(diffEntry)
+            .toEditList
+            .asScala
+            .toList
+            .filterNot(edit => edit.getType == Edit.Type.EMPTY)
+            .map(edit => new DiffData(DiffData.matchDiffDataType(edit.getType),
+              edit.getBeginA + 1,
+              edit.getBeginB + 1,
+              edit.getEndA + 1,
+              edit.getEndB + 1))))
         .toMap
     formatter.close()
     diffData
