61fdd656f1ffc16d99055f20c5fb1690ec577e30
Marco Arnaboldi <mr.arnaboldi@gmail.com>
2016-12-05 22:24:05 -0600

    Master work in progress

--- src/main/scala-2.11/edu/uic/cs474/project/Master.scala
+++ src/main/scala-2.11/edu/uic/cs474/project/Master.scala
@@ -1,13 +1,45 @@
 package edu.uic.cs474.project
 
-import akka.actor.Actor
+import akka.actor.{Actor, ActorRef, Props}
+import akka.routing.RoundRobinPool
+import edu.uic.cs474.project.Master.Init
+import edu.uic.cs474.project.downloading.ProjectDownloader
 
 /**
   * Created by andrea on 02/12/16.
   */
-class Master extends Actor {
+class Master(downloaders: Int, versioners: Int, parsers: Int) extends Actor {
 
-  /*def receive: Unit = {
+  var downloadersRouter: ActorRef = null
+  var parsersRouter: ActorRef = null
+  var versionersRouter: ActorRef = null
 
-  }*/
+
+  def receive = {
+
+    case Init => {
+      downloadersRouter = context.actorOf(
+        ProjectDownloader.props().withRouter(RoundRobinPool(downloaders)), name = "downloadersPool")
+
+      //TODO fix props
+      versionersRouter = context.actorOf(
+        ProjectDownloader.props().withRouter(RoundRobinPool(downloaders)), name = "downloadersPool")
+
+      //TODO fix props
+      parsersRouter = context.actorOf(
+        ProjectDownloader.props().withRouter(RoundRobinPool(downloaders)), name = "downloadersPool")
+    }
+
+  }
+}
+
+
+object Master
+{
+
+  trait Receive
+  case object Init extends Receive
+
+
+  def props(downloaders: Int, versioners: Int, parsers: Int): Props = Props(new Master(downloaders,versioners,parsers))
 }
