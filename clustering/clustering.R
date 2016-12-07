6cf3a37638d55132f44648d5a94095c6e16efbfa
Andrea Tirinzoni <andrea.tirinzoni@gmail.com>
2016-12-06 19:47:00 -0600

    Switched to Cosine similarity

--- clustering/clustering.R
+++ clustering/clustering.R
@@ -21,8 +21,30 @@ ids = which(sums==0)
 dataset_filtered = dataset[-ids,]
 
 #Remove duplicated samples
-dataset_filtered2 = unique(dataset_filtered)
+dataset_filtered = unique(dataset_filtered)
+
+#Define the cosine similarity
+cosineSimilarity = function(a,b) 
+{
+  return(sum(a*b)/sqrt(sum(a^2)*sum(b^2)))
+}   
+
+#Compute lower triangle of the cosine similarity matrix
+n = nrow(dataset_filtered)
+D = matrix(0,n,n)
+
+for(i in c(1:n)) {
+  for(j in c(1:n)) {
+    if(j<i) {
+      D[i,j] = cosineSimilarity(as.numeric(dataset_filtered[i,]),as.numeric(dataset_filtered[j,]))
+    }
+  }
+  print(i)
+}
+
+#Compute dist vector
+D = as.dist(D)
 
 #Apply hierarchical clustering
-clusters = hclust(dist(dataset_filtered2))
+clusters = hclust(D)
 plot(clusters)
